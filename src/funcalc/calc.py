a742e8c09d73683793899fa38f1a97a1246f2c4f
me <findaston@gmail.com>
2024-06-16 18:26:51 -0400

    -Added benchmarks to test speed of the two division functions.

--- src/funcalc/calc.py
+++ src/funcalc/calc.py
@@ -1,3 +1,6 @@
+import time
+
+
 def addition(x: float, y: float) -> float:
     z = x + y
     return z
@@ -14,6 +17,15 @@ def multiplication(x: float, y: float) -> float:
 
 
 def division(x: float, y: float) -> float:
+    if y == 0:
+        raise ValueError("Cannot divide by 0")
+    else:
+        z = x / y
+        return z
+
+
+def slow_division(x: float, y: float) -> float:
+    time.sleep(0.001)
     if y == 0:
         raise ValueError("Cannot divide by 0")
     else:
--- test/test_calc.py
+++ test/test_calc.py
@@ -1,6 +1,8 @@
+
+
 import pytest
 
-from funcalc.calc import addition, division, multiplication, subtraction
+from funcalc.calc import addition, division, multiplication, slow_division, subtraction
 
 
 def test_add() -> float:
@@ -24,8 +26,24 @@ def test_divi() -> float:
 
 
 def test_divi_0_error() -> None:
-    
     for x in range(100):
         y = 0
-        with pytest.raises(ValueError, match = "Cannot divide by 0"):
-            division(x,y)
+        with pytest.raises(ValueError, match="Cannot divide by 0"):
+            division(x, y)
+
+
+def test_slow_divi() -> float:
+    for x, y in zip(range(100), range(1, 100)):
+        assert slow_division(x, y) == x / y
+
+def test_slow_divi_0_error() -> None:
+    for x in range(100):
+        y = 0
+        with pytest.raises(ValueError, match="Cannot divide by 0"):
+            slow_division(x, y)
+
+def test_benchmark_slow_division(benchmark: None)-> None:
+    benchmark(slow_division,2,1)
+    
+def test_benchmark_division(benchmark: None)-> None:
+    benchmark(division,2,1)
